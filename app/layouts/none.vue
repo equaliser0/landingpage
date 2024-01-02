681ffcc3036895f82d4203175df5d7a66f4c4160
equaliser0 <accounts@equaliser.minimalcity.de>
2024-01-02 19:28:52 +0100

    add notification

--- app/layouts/none.vue
+++ app/layouts/none.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
+import { useNotificationStore } from '../stores/notifications'
+
+const notificationStore = useNotificationStore()
+
+// Global Notifications
+const getNotifcationType = (notificationType) => {
+  if (notificationType === 'standard') {
+    return 'bg-base-100';
+  };
+  if (notificationType === 'success') {
+    return 'alert-success';
+  };
+  if (notificationType === 'warning') {
+    return 'alert-warning';
+  };
+  if (notificationType === 'error') {
+    return 'alert-error';
+  };
+}
 </script>
 
 <template>
   <div ref="el" :style="{ 'font-family': 'Blinker' }" >
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Blinker">
+
+    <!-- Notification Toasts -->
+    <div class="toast toast-end z-30">
+      <div class="alert shadow" v-for="notification in notificationStore.notifications" :class="getNotifcationType(notification.type)" @click="notificationStore.closeNotification(notification)">
+        <svg v-if="notification.type === 'info'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-info shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+        <svg v-if="notification.type === 'success'" xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <svg v-if="notification.type === 'warning'" xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+        <svg v-if="notification.type === 'error'" xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <span>{{ notification.msg }}</span>
+      </div>
+    </div>
     <slot/>
   </div>
 </template>
